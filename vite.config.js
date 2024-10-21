4990bbaefe23a7eac16dbbab8c2370af32881361
kartheekreddy3 <karthikreddy49406@gmail.com>
2024-10-21 21:32:38 +0530

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  // Set the base URL for GitHub Pages deployment
-  base: "/app/", // Make sure this matches your GitHub repository name
-  plugins: [react()], // Use the React plugin
-});
